7a34b313d724a405994c368fa93915679eb429bc
Wyland Harris <wyland.harris@gmail.com>
2015-10-05 06:05:02 -0500

    Datatypes

---
+++ src/harris_3_datatypes/Harris_3_Datatypes.java
@@ -0,0 +1,43 @@
+
+package harris_3_datatypes;
+
+
+public class Harris_3_datatypes {
+
+    
+    public static void main(String[] args) {
+        //This is a comment
+        //We will use many comments to to explain our code
+        // this program explores some of the primitive data types
+        int i = 0;
+        float a= 0.01f;
+        double b = 0.012;
+        short c = (short) 0.0123;
+        char d = 123;
+        String e = "words";
+        boolean f = true;
+        
+        
+        /*
+        here we will look at some mathmatical operators
+        */
+        // + this adds to numbers and more
+        // - this subtracts two numbers and more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo it takes the remainder and more
+        
+        
+        
+        
+        System.out.println(i + " an intiger");
+        System.out.println(a + " a float");
+        System.out.println(b + " a double");
+        System.out.println(c + " a short");
+        System.out.println(d + " a char");
+        System.out.println(e + " a string");
+        System.out.println(f + " a boolean");
+
+    }
+    
+}
