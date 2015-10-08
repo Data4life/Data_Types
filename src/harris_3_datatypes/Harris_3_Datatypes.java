c8dec6dc88f2477893c0c24b1917764ff325a251
Wyland Harris <Data4life@users.noreply.github.com>
2015-10-08 10:54:34 -0500

    Update Harris_3_Datatypes.java

--- src/harris_3_datatypes/Harris_3_Datatypes.java
+++ src/harris_3_datatypes/Harris_3_Datatypes.java
@@ -13,10 +13,14 @@ public class Harris_3_datatypes {
         float a= 0.01f;
         double b = 0.012;
         short c = (short) 0.0123;
-        char d = 123;
-        String e = "words";
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4+"";
+        String e1 = "Amanda is the best football player!!!!!!!!!!!";
         boolean f = true;
-        
+        boolean g = i%2==0;
         
         /*
         here we will look at some mathmatical operators
@@ -34,10 +38,21 @@ public class Harris_3_datatypes {
         System.out.println(a + " a float");
         System.out.println(b + " a double");
         System.out.println(c + " a short");
-        System.out.println(d + " a char");
+        System.out.println(d1 + " a char");
+        System.out.println(d2 + " a char");
+        System.out.println(d3 + " a char");
+        System.out.println(d4 + " a char");
         System.out.println(e + " a string");
+        System.out.println(e1 + " a string");
         System.out.println(f + " a boolean");
-
+        System.out.println(g + " a boolean");
+//            for(i = -123; i < 123; i++) {
+//                System.out.println((char) i);
+        System.out.println(i + a +" an intiger /" + " a float");
+        System.out.println(a + b + " a float"+ " a double");
+        System.out.println(b + c + " a double"+ " a short");
+        System.out.println(c + " a short");
+            }
     }
     
 }
